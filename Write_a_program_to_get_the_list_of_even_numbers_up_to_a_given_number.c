6da5890f7c8f028bc7f744bf7956e00f03a2a049
Shamitha2005 <119730181+Shamitha2005@users.noreply.github.com>
2022-12-03 15:05:08 +0530

    #include<stdio.h> int main() {     int m,n,i;     scanf("%d%d",&n,&m);     for(i=n;i<=m;i++)     {         if (i%2==0)         printf("%d ",i);
        }
        
    }

---
+++ Write_a_program_to_get_the_list_of_even_numbers_up_to_a_given_number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,i;
+    scanf("%d%d",&n,&m);
+    for(i=n;i<=m;i++)
+    {
+        if (i%2==0)
+        printf("%d ",i);
+            
+    }
+    
+}
