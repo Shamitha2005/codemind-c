13df50d08804bf1bb67223f7e40d0ffe0996338b
Shamitha2005 <119730181+Shamitha2005@users.noreply.github.com>
2022-12-23 13:40:51 +0530

    #include<stdio.h> int main() {     int n,c;     scanf("%d",&n);     c=n/4;     if(n%4==0)     {         printf("%d",c);     }     else     {         printf("%d",c+1);     } }

---
+++ Cars.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,c;
+    scanf("%d",&n);
+    c=n/4;
+    if(n%4==0)
+    {
+        printf("%d",c);
+    }
+    else
+    {
+        printf("%d",c+1);
+    }
+}
